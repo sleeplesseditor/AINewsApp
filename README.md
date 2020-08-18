647987f352e19cdabaf48fa891036dff31f7ebdd
sleeplesseditor <josh@bryson.me.uk>
2020-08-18 14:48:08 +0100

    Connection to Alan AI SDK established

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # AI News App
 
-An experiment in building an accessible news application with the AI Alan and React, based on [JavaScript Mastery's tutorial](https://www.youtube.com/watch?v=rqw3OftE5sA&t=351s).
+An experiment in building an accessible news application with the AI Voice Assistant Alan and React, based on [JavaScript Mastery's tutorial](https://www.youtube.com/watch?v=rqw3OftE5sA&t=351s).
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: `${process.env.REACT_APP_ALAN_API_KEY}`,
+      onCommand: ({ command }) => {
+        if(command === 'testCommand') {
+          alert('This code was executed')
+        }
+      }
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Alan AI News App</h1>
     </div>
   );
 }
